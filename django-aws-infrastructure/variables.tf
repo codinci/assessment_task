77bf1a887b85ab88dc5ac05c03b86c0e41c41178
codinci <dkibau@gmail.com>
2024-09-29 08:37:04 +0300

    connected postgresql to ecs service

--- django-aws-infrastructure/variables.tf
+++ django-aws-infrastructure/variables.tf
@@ -17,3 +17,19 @@ variable "ecs_prod_backend_retention_days" {
   description = "Retention period for backend logs"
   default     = 30
 }
+
+variable "prod_rds_db_name" {
+  description = "RDS database name"
+  default     = "customer_orders_db"
+}
+variable "prod_rds_username" {
+  description = "RDS database username"
+  default     = "rds_user"
+}
+variable "prod_rds_password" {
+  description = "postgres password for production DB"
+}
+variable "prod_rds_instance_class" {
+  description = "RDS instance type"
+  default     = "db.t4g.micro"
+}
